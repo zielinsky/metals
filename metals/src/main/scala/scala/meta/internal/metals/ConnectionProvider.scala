74c48bb41e57c6de4119ed60440f6cbc3919068d
Tomasz Godzik <tgodzik@users.noreply.github.com>
2025-10-15 11:26:56 +0200

    bugfix: Readd build tool when missing (#7897)
    It seems if anyone removed their .metals directory, but left everything else, we would never set the used build tools again.
    This meant that when we check for sbt, we wouldn't get any result and we wouldn't support build.sbt file.
    
    Now, if build tool is missing we requery it.
    
    (cherry picked from commit 15679fab7986c1d0a1f385dc40d9482ed41f8774)

--- metals/src/main/scala/scala/meta/internal/metals/ConnectionProvider.scala
+++ metals/src/main/scala/scala/meta/internal/metals/ConnectionProvider.scala
@@ -771,13 +771,23 @@ class ConnectionProvider(
             "Connected to build server",
             true,
           ) {
-            bspConnector.connect(
-              buildToolProvider.buildTool,
-              folder,
-              () => userConfig,
-              shellRunner,
-              progress,
-            )
+            // If chosen build tool was removed at any point we want to readd it
+            val buildToolOpt: Future[Option[BuildTool]] =
+              buildToolProvider.buildTool match {
+                case Some(value) =>
+                  Future.successful(Some(value))
+                case None =>
+                  buildToolProvider.supportedBuildTool().map(_.map(_.buildTool))
+              }
+            buildToolOpt.flatMap { toolOpt =>
+              bspConnector.connect(
+                buildToolProvider.buildTool,
+                folder,
+                () => userConfig,
+                shellRunner,
+                progress,
+              )
+            }
           }
           .withInterrupt
         result <- maybeSession match {
